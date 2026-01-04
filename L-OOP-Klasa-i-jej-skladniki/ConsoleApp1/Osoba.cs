715a19605cae6908d48348aadce819ccb4a10e8f
Remigiusz <wasiak.r.study@gmail.com>
2026-01-04 20:39:23 +0100

    Add L-OOP-Klasa-i-jej-skladniki

---
+++ L-OOP-Klasa-i-jej-skladniki/ConsoleApp1/Osoba.cs
@@ -0,0 +1,62 @@
+﻿public class Osoba
+{
+
+    // dane
+    public string nazwisko;
+
+    private string imie;
+
+    public string Imie
+    {
+        get => imie;
+        private set => imie = value.Trim().ToUpper();
+    }
+
+    private string pesel;
+    private DateOnly dataUrodzenia;
+
+
+    public DateOnly DataUrodzenia
+    {
+        get => dataUrodzenia;
+        private set
+        {
+            if (value.Year < 1900)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value),
+                            "Data urodzenia nie może być wcześniejsza niż 1900");
+            }
+            dataUrodzenia = value;
+        }
+    }
+
+
+    // konstruktor
+    public Osoba(string nazwisko, string imie, DateOnly dataUrodzenia)
+    {
+        this.nazwisko = nazwisko;
+        this.imie = imie;
+        this.pesel = GenerujPESEL();
+        DataUrodzenia = dataUrodzenia;
+    }
+
+ 
+
+
+    // tekstowa reprezentacja obiektu
+    public override string ToString()
+    {
+        return $"{imie} {nazwisko}";
+    }
+
+    // metody
+
+    private string GenerujPESEL()
+    {
+        // DataUrodzenia = new DateOnly(2000,1,1);
+        return "12345678901";
+    }
+
+
+}
+
---
+++ L-OOP-Klasa-i-jej-skladniki/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+#nullable disable
+
+var o = new Osoba("Kowalski",
+                "Jan",
+                  //"012345678901",
+                new DateOnly(1901, 1, 1)
+                 );
+
+
+Console.WriteLine(o);
+o.nazwisko = "Abacki";
+Console.WriteLine(o);
+ //o.pesel = "12345";
+//Console.WriteLine(o + $" pesel: {o.pesel}");
