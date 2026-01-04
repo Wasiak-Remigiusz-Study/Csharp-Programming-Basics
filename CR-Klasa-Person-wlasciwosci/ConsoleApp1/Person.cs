601eaf2ce7a2c9258fbef9d76fa4773a8e6e582b
Remigiusz <wasiak.r.study@gmail.com>
2026-01-04 14:22:12 +0100

    Add tests from 1 to 8 for CR-Klasa-Person-wlasciwosci

---
+++ CR-Klasa-Person-wlasciwosci/ConsoleApp1/Person.cs
@@ -0,0 +1,109 @@
+﻿
+
+
+    public class Person
+    {
+
+
+        // dane
+        private string _familyName;
+        private string _firstName;
+        private DateTime _birthday;
+
+        public string FamilyName 
+        { 
+            get => _familyName; 
+            set
+            {
+                
+                if (value == null)
+                //if ( value.Length < 2 || value == null)
+                    {
+                    throw new ArgumentException("Incorrect data for FamilyName");
+                }
+                value = value.Trim();
+
+                if (value.Length < 2)
+                //if ( value.Length < 2 || value == null)
+                {
+                    throw new ArgumentException("Incorrect data for FamilyName");
+                }
+
+                var tab = value.Split('-', StringSplitOptions.RemoveEmptyEntries);
+                if (tab.Length > 2)
+                    throw new ArgumentException("Incorrect data for FamilyName");
+
+
+                // w zansiwsku tylko litery
+                foreach (var item in tab)
+                {
+                    var nazwisko = item;
+                    if (nazwisko.Length < 2)
+                        throw new ArgumentException("Incorrect data for FamilyName");
+
+                    foreach (var c in nazwisko)
+                    {
+                        if (!char.IsLetter(c))
+                            throw new ArgumentException("Incorrect data for FamilyName");
+                     }
+
+                // Pierwsza litera duza
+                    if (!char.IsUpper(nazwisko[0]))
+                        throw new ArgumentException("Incorrect data for FamilyName");
+                // Reszta male litery
+                    for (var i = 1; i < nazwisko.Length; i++)
+                    {
+
+                        if (!char.IsLetter(nazwisko[i]))
+                            throw new ArgumentException("Incorrect data for FamilyName");
+                    }
+                } 
+                _familyName = value;
+            }
+        }
+        public string FirstName 
+        { 
+            get => _firstName;
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException("Incorrect data for FamilyName");
+                }
+                _firstName = value;
+            }
+            
+        }
+        public DateTime Birthday 
+        {
+            get => _birthday;
+            set
+            {
+                _birthday = value;
+            }
+            
+        }
+
+
+        // konstruktor
+        public Person(string familyName, string firstName, DateTime birthday)
+        {
+            FamilyName = familyName;
+            FirstName = firstName;
+            Birthday = birthday;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {FamilyName} ({Birthday:yyy-MM-dd})"; ;
+        }
+
+
+
+
+
+        // metody - zachowanie obiektu
+
+    }
+
+
---
+++ CR-Klasa-Person-wlasciwosci/ConsoleApp1/Program.cs
@@ -0,0 +1,229 @@
+﻿// CR: Klasa Person (właściwości)
+
+// Zaprojektuj klasę Person spełniającą następujące założenia:
+
+// Dane:
+
+// FamilyName : string, nazwisko, tylko litery (co najmniej dwie), pierwsza litera duża, pozostałe małe; 
+// możliwość nazwiska dwuczłonowego - wtedy człony połączone łącznikiem (znak -), każdy człon z dużej litery, 
+// pozostałe z małych (człon nazwiska składa się z co najmniej 2. liter). Próba zapamiętania wartości 
+// niezgodnej z wytycznymi skutkuje zgłoszeniem wyjątku ArgumentException z komunikatem Incorrect data for FamilyName. 
+// Spacje na początku i końcu należy usunąć.
+
+// FirstName : string, imię, tylko litery (co najmniej dwie), pierwsza litera duża, pozostałe małe. 
+// Próba zapamiętania wartości niezgodnej z wytycznymi skutkuje zgłoszeniem wyjątku 
+// ArgumentException z komunikatem Incorrect data for FirstName. Spacje na początku i końcu należy usunąć.
+
+// Birthday : DateTime, data urodzenia, nie później niż dzisiaj. Próba zapamiętania wartości niezgodnej z wytycznymi 
+// skutkuje zgłoszeniem wyjątku ArgumentException z komunikatem Incorrect data for Birthday.
+
+// Dane obiektu są read/write oraz obligatoryjne, zrealizuj je jako właściwości (properties).
+
+// Zachowanie:
+
+// metoda ToString() wypisująca dane o osobie w formacie {FirstName} {FamilyName} ({Birthday}), data urodzenia w formacie yyyy-MM-dd.
+// Uwagi
+
+// Postaraj się zapisać kod klasy używając jak najmniejszej liczby linii kodu - np. wykorzystuj metody klasy string, jeśli 
+// kod miałby się powtarzać - zdefiniuj metodę prywatną, którą wykorzystasz wielokrotnie.
+
+// Do oceny przesyłasz kod klasy Person, ewentualnie poprzedzony dyrektywami using. Nie umieszczasz go w żadnej przestrzeni nazw. 
+// Twój kod (STUDENT_ANSWER) zostanie osadzony w podanym szkielecie, następnie całość zostanie skompilowana 
+// i uruchomiona - dla każdego przypadku testowego oddzielnie:
+
+
+
+using System;
+
+
+namespace CR_Klasa_Person_wlasciwosci
+{
+    public class Program
+    {
+        static void Test1()
+        {
+            /* Test: Utworzenie obiektu, dane poprawne */
+            var p1 = new Person(familyName: "Abacki",
+                                firstName: "John",
+                                birthday: new DateTime(year: 2000, month: 1, day: 1)
+                            );
+            Console.WriteLine(p1.FamilyName);
+            Console.WriteLine(p1.FirstName);
+            Console.WriteLine($"{p1.Birthday:yyyy-MM-dd}");
+        }
+
+        static void Test2()
+        {
+            /* Test: FamilyName, dane poprawne, nazwisko dwuczłonowe, ToString */
+            Person p2 = new Person("Abacka-Kuś",
+                                    "Ewa",
+                                    new DateTime(2000, 1, 1));
+            Console.WriteLine(p2);
+        }
+
+        static void Test3()
+        {
+            /* Test: FamilyName, dane błędne, jest null */
+            Person p3 = new Person("Aaa", "Bbb", new DateTime(2000, 1, 1));
+            try
+            {
+                p3.FamilyName = null;
+                Console.WriteLine(p3);
+            }
+            catch (ArgumentException e)
+            when (e.Message == "Incorrect data for FamilyName")
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void Test4()
+        {
+
+            /* Test: FamilyName, dane błędne, jest pusty string */
+            Person person4 = new Person("Aaa", "Bbb", new DateTime(2000, 1, 1));
+            try
+            {
+                person4.FamilyName = "";
+                //person4.FamilyName = null;
+                Console.WriteLine(person4.FamilyName);
+            }
+            catch (ArgumentException e)
+                when (e.Message == "Incorrect data for FamilyName")
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+        static void Test5()
+        {
+
+            /* Test: FamilyName, spacje przed i po nazwisku powinny być usunięte */
+            Person p5 = new Person("  Aaa  ", "Bbb", new DateTime(2000, 1, 1));
+            Console.WriteLine(p5.FamilyName);
+        }
+
+        static void Test6()
+        {
+            /* Test: FamilyName, za krótkie nazwisko, po usunięciu spacji */
+            Person p6 = new Person("  Aaa  ", "Bbb", new DateTime(2000, 1, 1));
+            try
+            {
+                p6.FamilyName = "  A  ";
+                Console.WriteLine(p6.FamilyName);
+            }
+            catch (ArgumentException e)
+                when (e.Message == "Incorrect data for FamilyName")
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+        static void Test7()
+        {
+
+            /* Test: FamilyName, nazwisko dwuczłonowe, za dużo członów */
+            Person p7 = new Person("Aaa", "Bbb", new DateTime(2000, 1, 1));
+            try
+            {
+                p7.FamilyName = "Aaa-Bbb-Ccc";
+                Console.WriteLine(p7.FamilyName);
+            }
+            catch (ArgumentException e)
+                when (e.Message == "Incorrect data for FamilyName")
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void Test8()
+        {
+            /* Test: FamilyName, nie dozwolone znaki */
+            Person p8 = new Person("Aaa", "Bbb", new DateTime(2000, 1, 1));
+            try
+            {
+                p8.FamilyName = "Aaa.Bbb1Ccc";
+                Console.WriteLine(p8.FamilyName);
+            }
+            catch (ArgumentException e)
+                when (e.Message == "Incorrect data for FamilyName")
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void Test9()
+        {
+            /* Test: FirstName, nie dozwolone znaki, null,
+            za krótkie po usunięciu spacji, pusty string,
+            */
+            Person p = new Person("Aaa", "Bbb", new DateTime(2000, 1, 1));
+            try
+            {
+                p.FirstName = "Aaa.Bbb1Ccc";
+                p.FirstName = null;
+                p.FirstName = "  A  ";
+                p.FirstName = "  ";
+                p.FirstName = "  A  ";
+                Console.WriteLine(p);
+            }
+            catch (ArgumentException e)
+                when (e.Message == "Incorrect data for FirstName")
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+        static void Test10()
+        {
+            /* Test: FamilyName, spacje przed i po imieniu powinny być usunięte */
+            Person p = new Person("Aaa", "   Bbb ", new DateTime(2000, 1, 1));
+            Console.WriteLine(p.FirstName);
+        }
+
+        static void Test11()
+        {
+            /* Test: FamilyName, spacje przed i po imieniu powinny być usunięte */
+            Person p = new Person("Aaa", "   Bbb ", new DateTime(2000, 1, 1));
+            Console.WriteLine(p.FirstName);
+        }
+
+        static void Test12()
+        {
+            /* Test: Birthday, data przyszła */
+            Person p = new Person("Aaa", "Bbb", new DateTime(2000, 1, 1));
+            try
+            {
+                p.Birthday = new DateTime(DateTime.Now.Year + 1, 1, 1);
+
+                Console.WriteLine(p);
+            }
+            catch (ArgumentException e)
+                when (e.Message == "Incorrect data for Birthday")
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            //Test1();
+            //Test2();
+            Test3();
+            //Test4();
+            //Test5();
+            Test6();
+            //Test7();
+            Test8();
+        }
+
+    }
+}
+
